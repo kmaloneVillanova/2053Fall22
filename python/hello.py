9085f8ac39efb44943999b7306c811ffab946896
kmaloneVillanova <kathleen.f.malone@villanova.edu>
2022-11-28 14:55:31 -0500

    added hello.py

---
+++ python/hello.py
@@ -0,0 +1,54 @@
+import math
+
+print("hello world!")
+# this is a comment
+'this is also a comment'
+'''
+Use this for several 
+lines of comments
+'''
+# code1
+# codepoint2namec
+# code 3
+# code 4
+
+int_example = 5
+float_example = 5.5
+
+result = 5.5 // 4
+print(result)
+
+result = 5.5 % 4
+print(result)
+
+squared = 5 ** 2
+print(squared)
+
+abs_value = abs(-5.5)
+print(abs_value)
+
+x = 10
+y = 100
+
+if x >= 10 or y <=100:
+    print("at least one statement is true")
+    print("this is a statement block")
+
+grade = 90
+if grade >= 90:
+    letter = "A"
+elif grade >= 80:
+    letter = "B"
+elif grade >= 70:
+    letter = "C"
+else:
+    letter="D"
+
+    
+
+
+
+
+
+
+
